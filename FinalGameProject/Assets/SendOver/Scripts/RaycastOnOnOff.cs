ac1ef8faf10b382dc5716a7771a280a8e2453660
StevenTuJH2022 <98783016+StevenTuJH2022@users.noreply.github.com>
2022-06-03 09:29:25 -0700

    CEO cutscene

---
+++ FinalGameProject/Assets/SendOver/Scripts/RaycastOnOnOff.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastOnOnOff : MonoBehaviour
+{
+    [SerializeField] private string selectableTag = "Selectable";
+    public GameObject On;
+    public GameObject On2;
+    public GameObject Off;
+
+    private Transform _selection;
+
+    private void Update()
+    {
+
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            var selection = hit.transform;
+            if (selection.CompareTag(selectableTag))
+            {
+                _selection = selection;
+                if (Input.GetMouseButton(0))
+                {
+                    Off.SetActive(false);
+                    On.SetActive(true);
+                    On2.SetActive(true);
+                }
+            }
+
+        }
+    }
+}
